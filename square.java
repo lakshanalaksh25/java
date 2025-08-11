22c1fa129eb897a57bc3cb384b4aa344b4a59179
lakshanalaksh25 <lakshanamlaks@gmail.com>
2025-08-11 14:43:21 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,15 @@
+class square {
+
+    int n=10;
+    void display(){
+        System.out.println(n*n);
+    }  
+
+
+
+public static void main(String[]args){
+
+    square s = new square();
+    s.display();
+}
+}
